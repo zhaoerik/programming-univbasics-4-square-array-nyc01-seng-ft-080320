19fd8434d8687ed8884a878b7ceed17bf93420e5
Erik Zhao <erikzhao98@gmail.com>
2020-07-20 23:05:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   # your code here
   count = 0
-  numbers = [1,2,3]
-  while count < numbers.length |index|
+  while count +=1 |index|
     numbers.index ** 2
   end
   square_array(array)
